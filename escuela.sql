bb1fad6c34578a0db55b9baa7000a2b218cc6b1c
harambeisnotdead <harambeisnotdead@users.noreply.github.com>
2017-02-23 10:18:56 +0100

    tablas creadas, faltan fk y inserts

--- escuela.sql
+++ escuela.sql
@@ -9,132 +9,132 @@ CREATE TABLE PROFESOR (
     apellidos varchar(30),
     numeroSS varchar(30),
     email varchar(30),
-	PRIMARY KEY(DNIProfesor)
+    PRIMARY KEY(DNIProfesor)
 );
 
 CREATE TABLE IMPARTE (
-	idAsignatura int,
+    idAsignatura int,
     DNIProfesor char(9),
-	PRIMARY KEY(idAsignatura, DNIProfesor)
+    PRIMARY KEY(idAsignatura, DNIProfesor)
 );
 
 CREATE TABLE TIENE (
-	idAsignatura int,
+    idAsignatura int,
     idAula int,
     idPlanta int,
     idGrupo int,
     idCurso int,
-	PRIMARY KEY(idAsignatura, idAula, idPlanta, idGrupo, idCurso)
+    PRIMARY KEY(idAsignatura, idAula, idPlanta, idGrupo, idCurso)
 );
 
 CREATE TABLE GRUPO (
-	idGrupo int,
+    idGrupo int,
     idCurso int,
     turno varchar(10),
-	PRIMARY KEY(idGrupo, idCurso)
+    PRIMARY KEY(idGrupo, idCurso)
 );
 
 CREATE TABLE CURSO (
-	idCurso int,
+    idCurso int,
     nombreCurso varchar(30),
-	PRIMARY KEY(idCurso)
+    PRIMARY KEY(idCurso)
 );
 
 CREATE TABLE PLANTA (
-	idPlanta int,
+    idPlanta int,
     cantidadAdulas int,
-	PRIMARY KEY(idPlanta)
+    PRIMARY KEY(idPlanta)
 );
 
 CREATE TABLE ASIGNATURA (
-	nombre varchar(30),
+    nombre varchar(30),
     idAsignatura int,
-	PRIMARY KEY(idAsignatura)
+    PRIMARY KEY(idAsignatura)
 );
 
 CREATE TABLE PERTENECE (
-	idDepartamento int,
+    idDepartamento int,
     DNIProfesor char(9),
-	PRIMARY KEY(idDepartamento, DNIProfesor)
+    PRIMARY KEY(idDepartamento, DNIProfesor)
 );
 
 CREATE TABLE UNIDAD (
-	idUnidad int,
+    idUnidad int,
     idAsignatura int,
-	PRIMARY KEY(idUnidad, idAsignatura)
+    PRIMARY KEY(idUnidad, idAsignatura)
 );
 
 CREATE TABLE DEPARTAMENTO (
-	idDepartamento int,
+    idDepartamento int,
     nombreDepartamento varchar(30),
     numeroProfesores int,
-	PRIMARY KEY(idDepartamento)
+    PRIMARY KEY(idDepartamento)
 );
 
 CREATE TABLE AULA (
-	idAula int,
+    idAula int,
     idPlanta int,
-	PRIMARY KEY(idAula, idPlanta)
+    PRIMARY KEY(idAula, idPlanta)
 );
 
 CREATE TABLE LIMPIA  (
-	idAula int,
+    idAula int,
     idPlanta int,
     DNIPersonalLimpieza char(9),
-	PRIMARY KEY(idAula, idPlanta, DNIPersonalLimpieza)
+    PRIMARY KEY(idAula, idPlanta, DNIPersonalLimpieza)
 );
 
 CREATE TABLE ES_EVALUADO (
-	DNIAlumno char(9),
+    DNIAlumno char(9),
     idUnidad int,
     idAsignatura int,
     nota int(3),
-	PRIMARY KEY(DNIAlumno, idUnidad, idAsignatura)
+    PRIMARY KEY(DNIAlumno, idUnidad, idAsignatura)
 );
 
 CREATE TABLE EVENTO (
-	idEvento int,
+    idEvento int,
     fecha date default "00-00-0000",
     hora varchar(5),
     descripcion varchar(50),
     idDepartamento int,
-	PRIMARY KEY(idEvento)
+    PRIMARY KEY(idEvento)
 );
 
 CREATE TABLE CONFERENCIA (
-	idEvento int,
+    idEvento int,
     nombreInvitado varchar(30),
     tema varchar(30),
     idAula int,
     idPlanta int,
-	PRIMARY KEY(idEvento)
+    PRIMARY KEY(idEvento)
 );
 
 CREATE TABLE EXCURSION (
-	idEvento int,
+    idEvento int,
     precio int,
     sitio varchar(30),
-	PRIMARY KEY(idEvento)
+    PRIMARY KEY(idEvento)
 );
 
 CREATE TABLE ALUMNO (
-	DNIAlumno char(9),
+    DNIAlumno char(9),
     DNIDelegado char(9),
     nombre varchar(30),
     apellidos varchar(50),
-	direccion varchar(40),
+    direccion varchar(40),
     fechaNacimiento date default "00-00-0000",
     email varchar(30),
     telefonoResponsable int(12),
-	idGrupo int,
-	idCurso int,
-	PRIMARY KEY(DNIAlumno)
+    idGrupo int,
+    idCurso int,
+    PRIMARY KEY(DNIAlumno)
 );
 
 CREATE TABLE PERSONAL_LIMPIEZA (
-	DNIPersonalLimpieza char(9),
+    DNIPersonalLimpieza char(9),
     nombre varchar(30),
     apellidos varchar(50),
     numeroSS varchar(15),
-	PRIMARY KEY(DNIPersonalLimpieza)
+    PRIMARY KEY(DNIPersonalLimpieza)
 );
