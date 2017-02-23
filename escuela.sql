0d9a5b0b476b2692dd97d5384445e8de630a2767
harambeisnotdead <harambeisnotdead@users.noreply.github.com>
2017-02-23 10:18:56 +0100

    tablas creadas, faltan fk y inserts

--- escuela.sql
+++ escuela.sql
@@ -3,138 +3,138 @@ CREATE DATABASE escuela;
 USE escuela;
 
 CREATE TABLE PROFESOR (
-	DNIProfesor char(9),
-    nombreProfesor varchar(30) default null,
+    DNIProfesor char(9),
+    nombre varchar(30),
     fechaNacimiento date default "00-00-0000",
-    apellido varchar(30),
+    apellidos varchar(30),
     numeroSS varchar(30),
     email varchar(30),
 	PRIMARY KEY(DNIProfesor)
-) ENGINE=INNODB; 
+);
 
 CREATE TABLE IMPARTE (
-	idAsignatura varchar(10),
+	idAsignatura int,
     DNIProfesor char(9),
 	PRIMARY KEY(idAsignatura, DNIProfesor)
-) ENGINE=INNODB; 
-
+);
 
 CREATE TABLE TIENE (
-	idAsignatura varchar(10),
-    idAula varchar(10),
-    idPlanta varchar(10),
-    idGrupo varchar(10),
-    idCurso varchar(10),
+	idAsignatura int,
+    idAula int,
+    idPlanta int,
+    idGrupo int,
+    idCurso int,
 	PRIMARY KEY(idAsignatura, idAula, idPlanta, idGrupo, idCurso)
-) ENGINE=INNODB; 
+);
 
 CREATE TABLE GRUPO (
-	idGrupo varchar(10),
-    idCurso varchar(10),
+	idGrupo int,
+    idCurso int,
     turno varchar(10),
 	PRIMARY KEY(idGrupo, idCurso)
-) ENGINE=INNODB; 
+);
 
 CREATE TABLE CURSO (
-	idCurso varchar(10),
+	idCurso int,
     nombreCurso varchar(30),
 	PRIMARY KEY(idCurso)
-) ENGINE=INNODB; 
+);
 
 CREATE TABLE PLANTA (
-	idPlanta varchar(10),
-    cantidadAdulas char(2) default 0,
+	idPlanta int,
+    cantidadAdulas int,
 	PRIMARY KEY(idPlanta)
-) ENGINE=INNODB; 
-
+);
 
 CREATE TABLE ASIGNATURA (
 	nombre varchar(30),
-    idAsignatura varchar(10),
+    idAsignatura int,
 	PRIMARY KEY(idAsignatura)
-) ENGINE=INNODB; 
-
+);
 
 CREATE TABLE PERTENECE (
-	idDepartamento varchar(10),
+	idDepartamento int,
     DNIProfesor char(9),
 	PRIMARY KEY(idDepartamento, DNIProfesor)
-) ENGINE=INNODB; 
+);
 
 CREATE TABLE UNIDAD (
-	idUnidad varchar(10),
-    idAsignatura varchar(10),
+	idUnidad int,
+    idAsignatura int,
 	PRIMARY KEY(idUnidad, idAsignatura)
-) ENGINE=INNODB; 
+);
 
 CREATE TABLE DEPARTAMENTO (
-	idDepartamento varchar(10),
+	idDepartamento int,
     nombreDepartamento varchar(30),
-    numeroProfesores varchar(3),
+    numeroProfesores int,
 	PRIMARY KEY(idDepartamento)
-) ENGINE=INNODB; 
+);
 
 CREATE TABLE AULA (
-	idAula varchar(10),
-    idPlanta varchar(10),
+	idAula int,
+    idPlanta int,
 	PRIMARY KEY(idAula, idPlanta)
-) ENGINE=INNODB; 
+);
 
 CREATE TABLE LIMPIA  (
-	idAula varchar(10),
-    idPlanta varchar(10),
+	idAula int,
+    idPlanta int,
     DNIPersonalLimpieza char(9),
 	PRIMARY KEY(idAula, idPlanta, DNIPersonalLimpieza)
-) ENGINE=INNODB; 
+);
 
-CREATE TABLE ESEVALUADO (
+CREATE TABLE ES_EVALUADO (
 	DNIAlumno char(9),
-    idUnidad varchar(10),
-    idAsignatura varchar(10),
-    nota varchar(3),
+    idUnidad int,
+    idAsignatura int,
+    nota int(3),
 	PRIMARY KEY(DNIAlumno, idUnidad, idAsignatura)
-) ENGINE=INNODB; 
+);
 
 CREATE TABLE EVENTO (
-	idEvento varchar(10),
-    fechaEvento date default "00-00-0000",
-    horaEvento varchar(5),
-    descripcionEvento varchar(50),
-    idDepartamento varchar(10),
-	PRIMARY KEY(idEvento, idDepartamento)
-) ENGINE=INNODB; 
+	idEvento int,
+    fecha date default "00-00-0000",
+    hora varchar(5),
+    descripcion varchar(50),
+    idDepartamento int,
+	PRIMARY KEY(idEvento)
+);
 
 CREATE TABLE CONFERENCIA (
-	idEvento varchar(10),
+	idEvento int,
     nombreInvitado varchar(30),
-    temaConferencia varchar(30),
-    idAula varchar(10),
-    idPlanta varchar(10),
-	PRIMARY KEY(idEvento, idAula, idPlanta)
-) ENGINE=INNODB; 
+    tema varchar(30),
+    idAula int,
+    idPlanta int,
+	PRIMARY KEY(idEvento)
+);
 
 CREATE TABLE EXCURSION (
-	idEvento varchar(10),
-    precio varchar(4),
+	idEvento int,
+    precio int,
     sitio varchar(30),
 	PRIMARY KEY(idEvento)
-) ENGINE=INNODB; 
+);
 
 CREATE TABLE ALUMNO (
 	DNIAlumno char(9),
     DNIDelegado char(9),
     nombre varchar(30),
     apellidos varchar(50),
+	direccion varchar(40),
     fechaNacimiento date default "00-00-0000",
     email varchar(30),
-    telefonoResponsable char(12),
+    telefonoResponsable int(12),
+	idGrupo int,
+	idCurso int,
 	PRIMARY KEY(DNIAlumno)
-) ENGINE=INNODB; 
+);
 
 CREATE TABLE PERSONAL_LIMPIEZA (
 	DNIPersonalLimpieza char(9),
-    nombrePL varchar(30),
-    apellidosPL varchar(50),
-    numeroSSPL varchar(15),
+    nombre varchar(30),
+    apellidos varchar(50),
+    numeroSS varchar(15),
 	PRIMARY KEY(DNIPersonalLimpieza)
-) ENGINE=INNODB; 
+);
