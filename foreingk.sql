50a7c52121d515a771522f27bc8a0934779e5abb
Joe J Gonzales V <joegv7@gmail.com>
2017-02-24 15:45:02 +0100

    Update foreingk.sql

--- foreingk.sql
+++ foreingk.sql
@@ -1,3 +1,6 @@
+alter table ALUMNO add constraint fk_es_evaluado_idGrupo foreign key (idGrupo) references GRUPO(idGrupo),
+            add constraint fk_es_evaluado_idCurso foreign key (idCurso) references GRUPO(idCurso);
+
 alter table ES_EVALUADO add constraint fk_es_evaluado_DNIAlumno foreign key (DNIAlumno) references ALUMNO(DNIAlumno),
             add constraint fk_es_evaluado_idUnidad foreign key (idUnidad) references idUnidad(UNIDAD);
             add constraint fk_es_evaluado_idAsignatura foreign key (idAsignatura) references idAsignatura(UNIDAD);
